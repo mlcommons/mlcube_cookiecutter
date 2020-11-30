508626f7931d9ef4daac97ab4eb3e03d72a64a19
Subin M <subin@apache.org>
2020-11-30 17:43:25 -0500

    cookie

--- {{cookiecutter.mlcube_name}}/build/{{cookiecutter.mlcube_name}}.py
+++ {{cookiecutter.mlcube_name}}/build/{{cookiecutter.mlcube_name}}.py
@@ -1,21 +1,31 @@
-""" Example entry point script compatible with MLCube protocol. """
+""" Example entry point script compatible with MLCube. """
 import argparse
+import yaml
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
 from enum import Enum
 from typing import List
 
 
 class Task(str, Enum):
-    EXAMPLE_TASK = 'example_task'
+    EXAMPLE_TASK = '{{cookiecutter.mlcube_name}}'
 
 
-def example_task(task_args: List[str]) -> None:
+def {{cookiecutter.mlcube_name}}(task_args: List[str]) -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument('--output_file', type=str, default=None, help="Name of an output file")
+    parser.add_argument('--parameters_file', '--parameters-file', type=str, required=True,
+                        help="Name of the parameter file.")
     args = parser.parse_args(args=task_args)
 
+    # Replace below lines with user implementation
+    with open(args.parameters_file) as stream:
+        config = yaml.load(stream.read(), Loader=Loader)
     with open(args.output_file, 'w') as stream:
-        stream.write("Hello from MLCube::example_task task.\n")
-
+        stream.write(f"Running {{cookiecutter.mlcube_name}} task with parameters '{config}'.\n")
+    
 
 def main():
     # Every MLCuber runner passes a task name as the first argument. Other arguments are task-specific.
@@ -27,7 +37,7 @@ def main():
     mlcube_args, task_args = parser.parse_known_args()
 
     if mlcube_args.mlcube_task == Task.EXAMPLE_TASK:
-        example_task(task_args)
+        {{cookiecutter.mlcube_name}}(task_args)
     else:
         raise ValueError(f"Unknown task: '{mlcube_args.mlcube_task}'")
 
